05c16d960c0687e46d2ab93ecea9c82c91856349
AbdAllah Abd El-fatah <AbdAllah.A.ELFatah@gmail.com>
2016-02-20 20:47:21 +0200

    finished cpy from/to USB, check TODOs for what to do nxt

--- auto-backup to USB app/Form1.cs
+++ auto-backup to USB app/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,36 @@ namespace auto_backup_to_USB_app
 {
     public partial class Form1 : Form
     {
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        private const int DBT_DEVTYP_VOLUME = 0x00000002;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    switch ((int)m.WParam)
+                    {
+                        case DBT_DEVICEARRIVAL:
+                            String fullPath = WriteToUSB.getFullPathToUSB();
+                            listBox1.Items.Add("D Arriverd");
+                            listBox1.Items.Add(fullPath);
+                            break;
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            listBox1.Items.Add("D Removed");
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
---
+++ auto-backup to USB app/USBOpertations/OperationsUSB_BaseClass.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace auto_backup_to_USB_app.USBOpertations
+{
+    class OperationsUSB_BaseClass
+    {
+        static DriveInfo[] drives;
+        static List<String> foldersToBackUp;
+
+        static OperationsUSB_BaseClass()
+        {
+            drives = DriveInfo.GetDrives();
+            foldersToBackUp = new List<String>();
+        }
+
+        static private string getFullPathToUSB()
+        {
+            string fullPath = string.Empty;
+            foreach (DriveInfo d in drives)
+            {
+                if (d.IsReady && d.DriveType == DriveType.Removable)
+                    fullPath = d.RootDirectory.FullName;
+            }
+            return fullPath;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="toUSB">ture to backup, false to restore</param>
+        static private void backUpData(bool toUSB)
+        {
+            string fileName, sourcePath = "", targetPath = "", destFile;
+
+            if (toUSB)
+            {
+                targetPath = getFullPathToUSB();
+            }
+            else
+            {
+                sourcePath = getFullPathToUSB();
+            }
+
+            foreach (String path in foldersToBackUp)
+            {
+                if (toUSB)
+                {
+                    sourcePath = path;
+                }
+                else
+                {
+                    targetPath = path;
+                }
+                if (!Directory.Exists(targetPath))
+                {
+                    Directory.CreateDirectory(targetPath);
+                }
+                string[] files = Directory.GetFiles(sourcePath);
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    fileName = Path.GetFileName(s);
+                    destFile =Path.Combine(targetPath, fileName);
+                    File.Copy(s, destFile, true);
+                }
+            }
+
+        }
+
+        static public void backupToUSB()
+        {
+            backUpData(true);
+        }
+
+        static public void storeFromUSB()
+        {
+            backUpData(false);
+        }
+
+        static public bool addFolderToList(String path)
+        {
+            bool existFolder = Directory.Exists(path);
+            if (existFolder) foldersToBackUp.Add(path);
+            return existFolder;
+        }
+
+        //TODO: learn how to start ur app in OS start up, and end it with it :D
+        //TODO: save list to txt file, and load from it in start up
+    }
+}
